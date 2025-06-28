77607823e81b118f24068ba100530780a145cc17
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-06-28 16:57:10 +0530

    Added Login and sign-up functionality

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+
+const Login = () => {
+
+  const [state, setState] = useState('Login');
+
+  const {setShowLogin} = useContext(AppContext)
+
+  // for diabling the scroll when the login form is open
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";// re-enable scroll when the component is unmounted
+    }
+  },[])
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-blue-50/30 flex justify-center items-center">
+      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          {state}
+        </h1>
+        <p className="text-sm">Welcome back! Please sign in to continue</p>
+
+        {state !== "Login" && (
+          <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.profile_icon} width={30} alt="" />
+            <input
+              className="outline-none text-sm"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        )}
+
+        <div className="border px-5 py-2 flex items-center gap-4 rounded-full mt-4">
+          <img src={assets.email_icon} alt="" />
+          <input
+            className="outline-none text-sm"
+            type="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="border px-5 py-2 flex items-center gap-4 rounded-full mt-4">
+          <img src={assets.lock_icon} alt="" />
+          <input
+            className="outline-none text-sm"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <p className="text-sm text-blue-600 cursor-pointer my-4">
+          Forgot password
+        </p>
+        <button
+          className="bg-blue-600 
+        hover:bg-blue-700 transition-all duration-300 
+        ease-in-out
+        w-full text-white py-2 rounded-full"
+        >
+          {state === "Login" ? "Login" : " Create Account"}
+        </button>
+        {state === "Login" ? (
+          <p className="mt-5 text-center">
+            Don't have an account?
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              Sign Up
+            </span>
+          </p>
+        ) : (
+          <p className="mt-5 text-center">
+            Already have an account?
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
+
+        {/* close the form */}
+        <img
+          onClick={() => {
+            setShowLogin(false);
+          }}
+          src={assets.cross_icon}
+          className="absolute top-5 cursor-pointer right-5"
+          alt=""
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
