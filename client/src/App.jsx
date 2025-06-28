0538caee1d08b5f19376be9cdef0a1053d32f9d0
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-06-28 16:59:29 +0530

    updation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,23 @@ import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './components/Login'
+import { AppContext } from './context/AppContext'
+import  { useContext } from "react";
+
 
 const App = () => {
+ const {showLogin} = useContext(AppContext);
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28  min-h-screen bg-gradient-to-b from-teal-50 to-orange-50' >
       {/* providing nevigation bar for all the pages */}
-      <Navbar/>
+      <Navbar />
+      {/* showing the login form if showLogin is true */}
+      {showLogin && <Login />}
+      {/* if showLogin is true then the login form will be shown */}
+      {/* otherwise the home page will be shown */}
+      
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
