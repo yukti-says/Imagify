17cdba2f1211615f174c61370ec7b2ca8afa6fac
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-07-03 15:49:50 +0530

    deploying

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
